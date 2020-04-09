c5f3ca4454f904f2f842f87713c993f9282e3d3a
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2020-04-09 20:33:59 +0200

    Add files via upload
    forras

---
+++ src/Kiiratas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FReni
+ */
+public class Kiiratas {
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        System.out.println("Figler Renáta");
+        System.out.print("Figler");
+        System.out.print(" Renáta");
+        System.out.println();
+    }
+}
